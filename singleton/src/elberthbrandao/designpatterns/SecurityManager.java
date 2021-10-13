557595b008d1a5df22b42e465b69d6b043d5a758
elberthbrandao <zelltr@hotmail.com>
2021-10-13 15:23:25 -0300

    singleton

---
+++ singleton/src/elberthbrandao/designpatterns/SecurityManager.java
@@ -0,0 +1,19 @@
+package elberthbrandao.designpatterns;
+
+public class SecurityManager {
+
+    private static SecurityManager instance;
+
+    private SecurityManager() {
+    }
+
+    public static SecurityManager getInstance() {
+        if(instance == null) {
+            instance = new SecurityManager();
+        }
+        return instance;
+    }
+
+    public void login() {
+    }
+}
---
+++ singleton/src/elberthbrandao/designpatterns/SecurityManager3.java
@@ -0,0 +1,23 @@
+package elberthbrandao.designpatterns;
+
+public class SecurityManager3 {
+
+    private static SecurityManager3 instance;
+
+    private SecurityManager3() {
+    }
+
+    public static SecurityManager3 getInstance() {
+        if(instance == null) {
+            synchronized(SecurityManager3.class) {
+                if(instance == null) {
+                    instance = new SecurityManager3();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void login() {
+    }
+}
